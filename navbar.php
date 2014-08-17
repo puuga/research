0721b9ca4831df131db6d4146211933a9639ca15
puuga <siwaoh@gmail.com>
2014-08-17 23:49:54 +0700

    fix ui, update research_view

--- navbar.php
+++ navbar.php
@@ -22,11 +22,11 @@
             System <span class="caret"></span>
           </a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="research_view.php"><span class="glyphicon glyphicon-pencil"></span> Research System</a></li>
+            <li><a href="research_view.php"><span class="glyphicon glyphicon-list-alt"></span> Research System</a></li>
             <li class="divider"></li>
-            <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Patent System</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-tag"></span> Patent System</a></li>
             <li class="divider"></li>
-            <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> Researcher System</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Researcher System</a></li>
           </ul>
         </li>
       </ul>
--- research_view.php
+++ research_view.php
@@ -25,7 +25,17 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <script>
 
+      function setDataForModal(title) {
+        $("#researchTitle").html(title);
+      }
+
+      function setDataToDelete(title) {
+        $("#researchTitleToDelete").html(title);
+      }
+
+    </script>
   </head>
   <body>
     <?php include 'navbar.php'; ?>
@@ -44,7 +54,7 @@
           <p></p>
           <p>
             <a class="btn btn-danger" href="input_form.php" role="button">
-              <span class="glyphicon glyphicon-pencil"></span> New Research
+              <span class="glyphicon glyphicon-plus"></span> New Research
             </a>
           </p>
         </div>
@@ -57,68 +67,47 @@
           <table class="table table-hover table-striped">
             <thead>
               <tr class="info">
-                <th>Department</th>
-                <th>year</th>
-                <th>Journal</th>
-                <th>Proceedings</th>
+                <th>Data</th>
+                <th>File</th>
+                <th>Edit</th>
+                <th>Delete</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td rowspan="2">Chemistry</td>
-                <td>2013</td>
-                <td>50</td>
-                <td>50</td>
-              </tr>
-              <tr>
-                <td>2014</td>
-                <td>60</td>
-                <td>60</td>
-              </tr>
-              <tr>
-                <td rowspan="2">Physics</td>
-                <td>2013</td>
-                <td>50</td>
-                <td>50</td>
-              </tr>
-              <tr>
-                <td>2014</td>
-                <td>60</td>
-                <td>60</td>
-              </tr>
-              <tr>
-                <td rowspan="2">Biology</td>
-                <td>2013</td>
-                <td>50</td>
-                <td>50</td>
-              </tr>
-              <tr>
-                <td>2014</td>
-                <td>60</td>
-                <td>60</td>
-              </tr>
-              <tr>
-                <td rowspan="2">Mathematics</td>
-                <td>2013</td>
-                <td>50</td>
-                <td>50</td>
-              </tr>
-              <tr>
-                <td>2014</td>
-                <td>60</td>
-                <td>60</td>
-              </tr>
-              <tr>
-                <td rowspan="2">CSIT</td>
-                <td>2013</td>
-                <td>50</td>
-                <td>50</td>
-              </tr>
-              <tr>
-                <td>2014</td>
-                <td>60</td>
-                <td>60</td>
-              </tr>
+              <?php
+                // set sql
+                $sql = "SELECT * FROM research";
+                $result = mysqli_query($con, $sql);
+                if (!$result) {
+                  die('Error: ' . mysqli_error($con));
+                } else {
+                  while($row = mysqli_fetch_array($result)) {
+                    ?>
+                    <tr>
+                      <td>
+                        <?php echo $row['reference']; ?>
+                      </td>
+                      <td>
+                        <a href='".$row['att_file']."'><span class='glyphicon glyphicon-download'></span> Download
+                      </td>
+
+                      <td>
+                        <!-- Button trigger modal -->
+                        <button class='btn btn-warning' data-toggle='modal' data-target='#myModal' onclick='setDataForModal("<?php echo $row['title']; ?>")'>
+                          <span class='glyphicon glyphicon-pencil'></span> edit
+                        </button>
+                      </td>
+                      <td>
+                        <!-- Button trigger modal alert -->
+                        <button class='btn btn-danger' data-toggle='modal' data-target='#alertToDelete' onclick='setDataToDelete("<?php echo $row['title']; ?>")'>
+                          <span class='glyphicon glyphicon-remove'></span> Delete
+                        </button>
+                      </td>
+                    </tr>
+                    <?php
+                  }
+                }
+              ?>
             </tbody>
           </table>
         </div>
@@ -126,6 +115,48 @@
 
     </div> <!-- end container -->
 
+
+    <!-- Modal -->
+    <div class="modal fade bs-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+          </div>
+          <div class="modal-body" id="mySmallModalBody">
+            <div>
+              <strong>Title:</strong> <span id="researchTitle"></span>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div> <!-- end Modal -->
+
+    <!-- Alert delete title -->
+    <!-- Small modal -->
+    <div class="modal fade bs-example-modal-sm" id="alertToDelete" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+            <h4 class="modal-title" id="mySmallModalLabel">Delete</h4>
+          </div>
+          <div class="modal-body" id="mySmallModalBody">
+            <strong>Title:</strong> <span id="researchTitleToDelete"></span>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cancle</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+
+      </div>
+    </div> <!-- end Alert -->
 
   </body>
 </html>
